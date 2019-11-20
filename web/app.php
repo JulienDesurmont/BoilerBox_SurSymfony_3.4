031a8840250c95cbcb9756da6aeeb66086bc6ba2
Julien <j.desurmont@lci-group.fr>
2019-11-20 11:32:06 +0100

    Mise en maintenance

--- web/app.php
+++ web/app.php
@@ -1,6 +1,17 @@
 <?php
-
 use Symfony\Component\HttpFoundation\Request;
+
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
+// '185.30.209.18' est l'adresse Carvin de developpement
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server' || '185.30.209.18')
+) {
+    header('HTTP/1.0 403 Forbidden');
+	exit('Le site est en maintenance. Veuillez nous excuser pour la gène occasionnée.');
+}
+
 
 require __DIR__.'/../vendor/autoload.php';
 if (PHP_VERSION_ID < 70000) {
